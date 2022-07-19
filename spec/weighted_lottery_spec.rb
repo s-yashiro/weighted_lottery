ae674c0069fdefc3df63903442346e79b47f71c4
s-yashiro <satoshi.yashiro@gmail.com>
2022-07-19 17:03:45 +0900

    add Rspec

--- spec/weighted_lottery_spec.rb
+++ spec/weighted_lottery_spec.rb
@@ -3,7 +3,38 @@ RSpec.describe WeightedLottery do
     expect(WeightedLottery::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "Random targets should appear in accordance with the dividend" do
+    let(:items) do
+      {
+        "big_win" => 10,
+        "win" => 20,
+        "loose" => 70 
+      }
+    end
+    context "win/loose appears more than big_win" do
+      let(:result) {
+        big_win, win, loose = [0, 0, 0]
+        100.times do |i|
+          case WeightedLottery.pick(items).to_sym
+          when :big_win
+            big_win += 1
+          when :win
+            win += 1
+          when :loose
+            loose += 1
+          else
+          end
+        end
+        {
+          "big_win" => big_win,
+          "win" => win,
+          "loose" => loose
+        }
+      }
+
+      it {expect(result["win"]).to be >= result["big_win"]}
+      it {expect(result["loose"]).to be >= result["big_win"]}
+    end
   end
+
 end
